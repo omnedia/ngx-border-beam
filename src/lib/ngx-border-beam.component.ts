963b6d1e1f61f9455e3cfe8db076c940c5c15337
Markus Block <markus.block@y-com-automation.de>
2024-08-14 09:41:16 +0200

    Init Library

---
+++ src/lib/ngx-border-beam.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "om-border-beam",
+  standalone: true,
+  imports: [],
+  templateUrl: "./ngx-border-beam.component.html",
+  styleUrl: "./ngx-border-beam.component.scss",
+})
+export class NgxBorderBeamComponent {}
---
+++ src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-border-beam
+ */
+
+export * from "./lib/ngx-border-beam.component";
